8de5fc609da59df915d3f8d390abb26b647af214
Amal Ajay <amala@dexlock.com>
2022-02-03 11:08:36 +0530

    Image Prediction from Posenet

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Pose-Estimation-TFLite-
+# Pose-Estimation-TFLite
+
+Install the dependencies
+```bash
+pip install -r requirements.txt
+```
+
+Run Pose Estimation on Image
+```bash
+python posenet_tflite.py
+```
+
+Result 
+
+![Result](result.png)
---
+++ posenet_tflite.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+import numpy as np
+from helper import *
+from matplotlib import pyplot as plt
+
+# Input size for the model 
+input_size = 256
+
+def predict_movenet_for_image(input_image):
+
+    interpreter = tf.lite.Interpreter(model_path="lite-model_movenet_singlepose_thunder_tflite_float16_4.tflite")
+
+    interpreter.allocate_tensors()
+    """Runs detection on an input image.
+
+    Args:
+        input_image: A [1, height, width, 3] tensor represents the input image
+        pixels. Note that the height/width should already be resized and match the
+        expected input resolution of the model before passing into this function.
+
+    Returns:
+        A [1, 1, 17, 3] float numpy array representing the predicted keypoint
+        coordinates and scores.
+    """
+    # TF Lite format expects tensor type of uint8.
+    input_image = tf.cast(input_image, dtype=tf.uint8)
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    interpreter.set_tensor(input_details[0]['index'], input_image.numpy())
+    # Invoke inference.
+    interpreter.invoke()
+    # Get the model prediction.
+    keypoints_with_scores = interpreter.get_tensor(output_details[0]['index'])
+
+    return keypoints_with_scores
+
+# Load the input image.
+image_path = 'image/img1.jpg'
+image = tf.io.read_file(image_path)
+image = tf.image.decode_jpeg(image)
+
+# Resize and pad the image to keep the aspect ratio and fit the expected size.
+input_image = tf.expand_dims(image, axis=0)
+input_image = tf.image.resize_with_pad(input_image, input_size, input_size)
+
+
+# Run model inference.
+keypoints_with_scores = predict_movenet_for_image(input_image)
+
+# Visualize the predictions with image.
+display_image = tf.expand_dims(image, axis=0)
+display_image = tf.cast(tf.image.resize_with_pad(
+    display_image, 1280, 1280), dtype=tf.int32)
+output_overlay = draw_prediction_on_image(
+    np.squeeze(display_image.numpy(), axis=0), keypoints_with_scores)
+
+plt.figure(figsize=(5, 5))
+plt.imshow(output_overlay)
+plt.savefig('result.png')
+_ = plt.axis('off')
